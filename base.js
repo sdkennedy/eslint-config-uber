85941a65458cb1d25ad6d9a8795b4d96e1e9cde8
Scott Kennedy <skennedy@uber.com>
2015-11-30 13:32:59 -0800

    Added better detection of editorconfig indentation

--- base.js
+++ base.js
@@ -1,6 +1,6 @@
 var readFileSync = require('fs').readFileSync;
 var path = require('path');
-var ini = require('ini');
+var editorconfig = require('editorconfig');
 
 var basePath = path.join('uber-standard', 'rc', '.eslintrc.json');
 var baseRules = require(basePath);
@@ -11,18 +11,19 @@ Object.keys(baseRules).forEach(function(key){
 });
 
 // Attempt to pull indent size from project root's .editorconfig [*]
-try {
-  var editorConfigPath = path.join('.', '.editorconfig');
-  var editorConfig = ini.parse(readFileSync(editorConfigPath, 'utf-8'));
-  var indentSize = editorConfig && editorConfig['*'] && editorConfig['*'].indent_size;
-  if(typeof indentSize === 'string'){
-    rules.rules = rules.rules || {};
-    rules.rules.indent = rules.rules.indent || [2, 2];
-    rules.rules.indent[1] = parseInt(indentSize, 10);
-  }
-} catch (err) {
-  // Do nothing
+var editorConfig;
+editorConfig = editorconfig.parseSync('.editorconfig');
+if( Object.keys(editorConfig).length === 0 ){
+  editorConfig = editorconfig.parseSync('.');
 }
+
+var indentSize = editorConfig.indent_size;
+if(typeof indentSize === 'number'){
+  rules.rules = rules.rules || {};
+  rules.rules.indent = rules.rules.indent || [2, 2];
+  rules.rules.indent[1] = indentSize;
+}
+
 // Haven't figured out how to include custom rules in a reusable config
 delete rules.rules['no-try-catch'];
 
